b9816ec569d84bb4b6c90f3dddf6cfbcbe403b30
Caner Derici <canerderici@gmail.com>
2019-05-30 14:09:01 -0400

    change symbols for environment and store

--- main.rkt
+++ main.rkt
@@ -10,7 +10,7 @@
        (lambda (x_!_ ...) e) (let-values (((x_!_) e) ...) e)
        (raises e)] ;; expressiosn
   [v   ::= n b c (void)] ;; values
-  [c   ::= (closure x ... e Σ)]
+  [c   ::= (closure x ... e ε)]
   [n   ::= number]
   [b   ::= true false]
   [x cell ::= variable-not-otherwise-mentioned] ;; variables
@@ -21,16 +21,15 @@
        (begin v ... E e ...) (set! x E)
        (let-values (((x) v) ... ((x) E) ((x) e) ...) e)] ;; eval context
 
-  [Σ   ::= ((x any) ...)] ;; environment
-  [σ   ::= ((x any) ...)] ;; store
+  [ε   ::= ((x any) ...)] ;; environment
+  [Σ   ::= ((x any) ...)] ;; store
 
   [e-test ::= x n b (void) 
           (e-test e-test ...) (lambda (x_!_ ...) e-test) (if e-test e-test e-test) 
           (p2 e-test e-test) (p1 e-test) (set! x e-test) (begin e-test e-test ...) 
           (let-values (((x) e-test) ...) e-test) (raises e-test)] ;; to be used to generate test cases (i.e. exclude closures)
 
-  [rc-result ::= v closure stuck]
-  [rc-out ::= v stuck]
+  [rc-result ::= v stuck]
   [exp-id ::= x (x x)]
   [C ::= cell uninit]
 
@@ -61,10 +60,10 @@
    (check-the-current-instance any_1 any_2)])
 
 (define-metafunction RC
-  interpret-stack : e Σ σ -> rc-result
-  [(interpret-stack rc-result Σ σ) rc-result]
-  [(interpret-stack (raises e) Σ σ) stuck]
-  [(interpret-stack x Σ σ) (lookup Σ x)])
+  interpret-stack : e ε Σ -> rc-result
+  [(interpret-stack rc-result ε Σ) rc-result]
+  [(interpret-stack (raises e) ε Σ) stuck]
+  [(interpret-stack x ε Σ) (lookup Σ (lookup ε x))])
 
 (define-metafunction RC
   eval-stackful : e -> rc-result
--- tests.rkt
+++ tests.rkt
@@ -4,3 +4,4 @@
 
 (test-equal (term (eval-stackful 2)) 2)
 (test-equal (term (eval-stackful (raises 1))) (term stuck))
+(test-equal (term (interpret-stack a ((a s0) (b s1)) ((s0 1) (s1 2)))) 1)
